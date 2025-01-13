47dad2f1ff8f072e7f676bbd7f6e9a22d2e8d855
Cyclonekillzyt <kendrickscyclone@gmail.com>
2025-01-13 22:27:08 +0000

    finally done with the grid pattern

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
 const container = document.getElementById("container");
-for (let i = 0; i < 16; i++) {
-  for (let j = 0; j < 16; j++) {
-    const etchASketch = document.createElement("div");
-    etchASketch.classList.add("etchASketch");
-    container.appendChild(etchASketch);
-  }
+const etchASketchArray = [];
+
+
+
+function createGrid(){
+    for (let i = 0; i < 14; i++) {
+        for (let j = 0; j < 14; j++) {
+          const etchASketch = document.createElement("div");
+          etchASketch.style.width = "30px";
+          etchASketch.style.height = "30px";
+          etchASketch.style.border = "1px solid black";
+          container.appendChild(etchASketch);
+          etchASketchArray.push(etchASketch);
+        }
+      }
 }
+
+
+etchASketchArray.forEach((etchASketch) => {
+    etchASketch.addEventListener("mouseover", () => {
+        etchASketch.style.backgroundColor = "black";
+    });
+});
+
+
+
 
