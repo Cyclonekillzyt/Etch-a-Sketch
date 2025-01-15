e1cc1975b6f5f6b342eeea1e590bfcd1a0bf033d
Cyclonekillzyt <kendrickscyclone@gmail.com>
2025-01-15 21:33:12 +0000

    Took a break , im back now

--- script.js
+++ script.js
@@ -3,51 +3,41 @@ const resizeButton = document.getElementById("resize");
 const randomColorBtn = document.getElementById("randomColor");
 const blackColorBtn = document.getElementById("blackColor");
 const resetBtn = document.getElementById("reset");
+const containerWidth = container.offsetWidth;
 const etchASketchArray = [];
-let gridSize = 16;
+let gridSize = 36;
 
-document.addEventListener("DOMContentLoaded", () => {
-    createGrid(gridSize);
-});
+document.onload = createGrid(gridSize);
 
 resizeButton.addEventListener("click", () => {
-    container.innerHTML = "";
-    etchASketchArray.length = 0;
-    gridSize = parseInt(prompt("Enter the size of the grid"));
-    if(gridSize > 1 && gridSize < 100){
-        createGrid(gridSize);
-    }
-    else{
-        alert("Please enter a number between 1 and 100");
-        gridSize = 16;
-        createGrid(gridSize);
-    }
+  container.innerHTML = "";
+  etchASketchArray.length = 0;
+  gridSize = parseInt(prompt("Enter the size of the grid"));
+  if (gridSize > 1 && gridSize < 100) {
+    createGrid(gridSize);
+  } else {
+    alert("Please enter a number between 1 and 100");
+    gridSize = 16;
+    createGrid(gridSize);
+  }
 });
 
+function createGrid(size) {
+  let totalSize = size * size;
+  for (let i = 0; i <= totalSize; i++) {
+    const etchASketch = document.createElement("div");
+    etchASketch.style.display = "flex";
+    etchASketch.style.flex = `0 0 ${squareSize}px`;
+    etchASketch.style.border = `1px solid`;
+    etchASketch.style.boxSizing = "border-box";
+    container.style.overflow = "hidden";
 
-function createGrid(size){
-    const squareSize = Math.floor(500 / size);
-    for (let i = 0; i < size; i++) {
-        for (let j = 0; j < size; j++) {
-          const etchASketch = document.createElement("div");
-          etchASketch.style.width = `${squareSize}px`;
-          etchASketch.style.height = `${squareSize}px`;
-          etchASketch.style.border = "1px solid black";
-          etchASketch.style.overflowX = "hidden";
-          container.appendChild(etchASketch);
-          etchASketchArray.push(etchASketch);
-        }
-      }
-      etchASketchArray.forEach((etchASketch) => {
-        etchASketch.addEventListener("mouseover", () => {
-            etchASketch.style.backgroundColor = "black";
-        });
+    container.appendChild(etchASketch);
+    etchASketchArray.push(etchASketch);
+  }
+  etchASketchArray.forEach((etchASketch) => {
+    etchASketch.addEventListener("mouseover", () => {
+      etchASketch.style.backgroundColor = "black";
     });
-};
-
-
-
-
-
-
-
+  });
+}
